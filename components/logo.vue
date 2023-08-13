17825e743b14de523c2eff3a87e882fbc326ef7f
vetal7777 <jobschildren@gmail.com>
2023-08-13 18:13:47 +0300

    feat: dynamic size logo

--- components/logo.vue
+++ components/logo.vue
@@ -1,16 +1,32 @@
 <template>
   <div
-    class="relative mb-5 flex w-min"
+    class="relative flex w-min"
     :class="{
-      'scale-100': size === 'xl',
-      'scale-90': size === 'l',
-      'scale-75': size === 'm',
-      'scale-50': size === 's'
+      'mb-5': currentSize.xl,
+      'mb-[1.125rem]': currentSize.l,
+      'mb-[0.94rem]': currentSize.m,
+      'mb-[0.625rem]': currentSize.s
     }"
   >
-    <div class="text-5xl text-white">seven</div>
     <div
-      class="text-s absolute bottom-[-16px] right-[-30px] rounded-xl bg-white px-[6px] text-black"
+      class="text-white"
+      :class="{
+        'text-5xl': currentSize.xl,
+        'text-[2.7rem] leading-[0.9]': currentSize.l,
+        'text-[2.25rem] leading-[0.75]': currentSize.m,
+        'text-[1.5rem] leading-[0.5]': currentSize.s
+      }"
+    >
+      seven
+    </div>
+    <div
+      class="absolute bottom-[-16px] rounded-xl bg-white text-black"
+      :class="{
+        'right-[-30px] px-[6px] text-base': currentSize.xl,
+        'right-[-27px] px-[5.4px] text-[0.9rem]': currentSize.l,
+        'right-[-22.5px] px-[4.5px] text-[0.75rem]': currentSize.m,
+        'right-[-15px] px-[3px] text-[0.5rem]': currentSize.s
+      }"
     >
       wallet
     </div>
@@ -18,10 +34,20 @@
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { LogoSizes } from '@/types'
+
+const props = defineProps({
   size: {
-    type: String,
-    default: 'xl'
+    type: String as PropType<LogoSizes>,
+    default: 'xl',
+    require: false
   }
+})
+
+const currentSize = ref({
+  xl: props.size === 'xl',
+  l: props.size === 'l',
+  m: props.size === 'm',
+  s: props.size === 's'
 })
 </script>
