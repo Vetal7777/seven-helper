91aa7084a52729df1a62ac613f0bbf5ef1572823
vetal7777 <jobschildren@gmail.com>
2023-08-19 01:34:48 +0300

    fix: logo on hover transition bug

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center p-4 text-white">
-    <Logo class="cursor-pointer" @click="navigateTo(ROUTES.home)" />
+    <Logo class="cursor-pointer hover:ml-1" @click="navigateTo(ROUTES.home)" />
   </div>
 </template>
 
