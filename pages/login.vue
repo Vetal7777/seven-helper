b22a57f6c9262832ffe8029b9b439abfe7476994
vetal7777 <jobschildren@gmail.com>
2023-08-19 01:29:09 +0300

    feat: set login page transition

--- pages/login.vue
+++ pages/login.vue
@@ -4,14 +4,37 @@
       class="flex h-screen grow items-center justify-center bg-login bg-cover"
     >
       <div
-        class="flex basis-2/6 flex-col gap-5 rounded-2xl bg-gradient-to-br from-gray-700 to-black p-8"
+        class="flex flex-col items-center gap-5 rounded-2xl bg-gradient-to-br from-gray-700 to-black p-10"
       >
-        <Logo size="m" />
+        <Logo size="xl" />
+
+        <BaseInput placeholder="Email" label="Email" />
+        <BaseInput
+          :type="inputTypes.password"
+          placeholder="Password"
+          label="Password"
+        />
+        <div class="text-xs text-white">
+          Doesn't have an account?
+          <button
+            class="text-slate-400 duration-200 ease-out hover:text-slate-300"
+          >
+            Sign in
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inputTypes } from '@/utils'
+
+definePageMeta({
+  pageTransition: {
+    name: 'rotate'
+  }
+})
+</script>
 
 <style lang="scss" scoped></style>
