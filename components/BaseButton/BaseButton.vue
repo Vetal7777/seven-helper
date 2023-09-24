178015df6ebc74a0ebb6d9cb005497d027a8eb6c
vetal7777 <jobschildren@gmail.com>
2023-09-24 15:25:15 +0300

    feat: create base button

---
+++ components/BaseButton/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="rounded-lg bg-white px-4 py-3 outline-none duration-200 ease-in-out hover:scale-110"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String
+})
+</script>
