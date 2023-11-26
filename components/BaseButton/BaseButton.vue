828eb1cfd3bab0f9a2bc16f2455c6ddf59c2ed00
vetal7777 <jobschildren@gmail.com>
2023-11-26 04:55:00 +0200

    feat: update BaseButton.vue

--- components/BaseButton/BaseButton.vue
+++ components/BaseButton/BaseButton.vue
@@ -2,12 +2,6 @@
   <button
     class="rounded-lg bg-white px-4 py-3 outline-none duration-200 ease-in-out hover:scale-110"
   >
-    {{ title }}
+    <slot name="title"></slot>
   </button>
 </template>
-
-<script setup lang="ts">
-defineProps({
-  title: String
-})
-</script>
